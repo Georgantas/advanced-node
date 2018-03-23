0540a7f8ac7f2b86a5ef9404bdcea307f607c2aa
Georgantas <john.georan@gmail.com>
2018-03-23 14:19:18 -0400

    prog

--- AdvancedNodeStarter/tests/helpers/page.js
+++ AdvancedNodeStarter/tests/helpers/page.js
@@ -42,6 +42,40 @@ class CustomPage { // could be called Page instead
     async getContentsOf(selector) {
         return this.page.$eval(selector, el => el.innerHTML);
     }
+
+    get(path) {
+        return this.page.evaluate((_path) => {
+            return fetch(_path, {
+                method: 'GET',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.json());
+        }, path);
+        
+    }
+
+    post(path, data) {
+        return this.page.evaluate((_path, _data) => {
+            // NOTE: THE FOLLOWING GETS TURNED TO A STRING, SO WE CANT REFERENCE "PATH" DIRECTLY
+            // That's why it's provided as a second argument.
+            return fetch(_path, {
+                method: 'POST',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(_data)
+            }).then(res => res.json());
+        }, path, data);
+    }
+
+    execRequests(actions){
+        return Promise.all(actions.map(({ method, path, data }) => {
+            return this[method](path, data);
+        }));
+    }
 }
 
 module.exports = CustomPage;
